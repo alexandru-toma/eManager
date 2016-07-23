b6ccdc46cdfd7e26058114deee9d670356a84b3d
Alexandru Toma <alexandru.toma@bt.com>
2016-07-23 19:07:46 +0100

    First commit

---
+++ eManager/eManager.Domain/IDepartmentDataSource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eManager.Domain
+{
+    public interface IDepartmentDataSource
+    {
+        IQueryable<Employee> Employees { get; }
+        IQueryable<Department> Departments { get; }
+
+        void Save();
+    }
+ 
+
+}
